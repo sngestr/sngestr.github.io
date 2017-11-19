3cb2c336ac03739ab0e0afc076cfe4cec69133fe
Esther Song <esther.song62@myhunter.cuny.edu>
2017-11-19 03:52:12 -0500

    nav bar works now

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 $(window).on('load', function() {
     $("#cover").hide();
 });
+
+$(document).ready(function() {
+	$('#main').html($('#main_section_home').html());
+});
+
+function show(param_div_id) {
+	$('#main').html($('#' + param_div_id).html());
+}
