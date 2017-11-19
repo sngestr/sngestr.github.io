5e6e62d437910de9bb227b676757b4ab7dd4b1b5
Esther Song <esther.song62@myhunter.cuny.edu>
2017-11-19 04:56:50 -0500

    fix styling and add fading effect

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ $(window).on('load', function() {
 });
 
 $(document).ready(function() {
+	$('#wrapper').fadeIn();
 	$('#main').html($('#main_section_home').html());
 });
 
 function show(param_div_id) {
-	$('#main').html($('#' + param_div_id).html());
+	$('#main').fadeOut('normal', function() {
+      $('#main').html($('#' + param_div_id).html());
+      $('#main').fadeIn('normal');
+  });
 }
